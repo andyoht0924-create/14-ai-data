d8447926635e9727bf9368a6f902377c4203673a
andyoht0924 <andyoht0924@gmail.com>
2026-04-02 21:01:56 +0900

    260402 21:00

---
+++ 06_scrapper/main.py
@@ -0,0 +1,36 @@
+# pip install requests
+# pip install beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+
+def search_incruit(keyword):
+    page = 3
+    jobs = []
+    
+    for i in range(page):
+        page = i * 30
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+
+        response = requests.get(url)
+        # print(response.content)
+        soup = BeautifulSoup(response.text, "html.parser")
+        # print(soup.prettify)
+        lis = soup.find_all("li", class_="c_col")
+        # print(lis)
+        # print(len(lis))
+        for li in lis:
+            title = li.find("div", class_="cell_mid").find("a").text
+            company = li.find("a", class_="cpname").text
+            href = li.find("div", class_="cell_mid").find("a").get("href")
+            location = li.find("div", class_="cl_md").find_all("span")[0].text
+
+            job_data = {
+                "company" : company,
+                "title" : title,
+                "location" : location,
+                "href" : href
+            }
+
+            jobs.append(job_data)
+
+    return(jobs)
